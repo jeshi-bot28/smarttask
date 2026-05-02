25d4202a268f921c311b7cd8c4921d4ff82bbe2e
jeshi-bot28 <wick22cx@gmail.com>
2026-05-02 14:18:40 +0800

    Update task_service.py

--- models/services/task_service.py
+++ models/services/task_service.py
@@ -18,3 +18,10 @@ class TaskService:
 
     def get_all_tasks(self):
         return self.tasks
+
+
+def edit_task(self, old_title, new_title, new_category):
+    for task in self.tasks:
+        if task.title == old_title:
+            task.title = new_title
+            task.category = new_category
