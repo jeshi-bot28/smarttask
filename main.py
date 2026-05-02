40e2c64e5a86647255713bb463fa4a01071a5486
jeshi-bot28 <wick22cx@gmail.com>
2026-05-02 14:20:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,28 @@ from services.task_service import TaskService
 
 service = TaskService()
 
+# Add tasks
 service.add_task("Homework", "School")
 service.add_task("Workout", "Personal")
+service.add_task("Project", "Work")
 
+# Edit task
+service.edit_task("Project", "Final Project", "School")
+
+# Mark complete
 service.mark_completed("Homework")
 
+# Show all tasks
+print("ALL TASKS:")
 for task in service.get_all_tasks():
+    print(task)
+
+# Filter by category
+print("\nSCHOOL TASKS:")
+for task in service.filter_by_category("School"):
+    print(task)
+
+# Filter by completed
+print("\nCOMPLETED TASKS:")
+for task in service.filter_by_status(True):
     print(task)
